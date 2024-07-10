a2fd3a9473ba2330f7d36e4151e9ddf2bb58841b
navya0598 <navyap1398@gmail.com>
2024-07-10 07:48:29 +0530

    Commited

---
+++ src/JavaPackage/FibonacciSeries.java
@@ -0,0 +1,36 @@
+package JavaPrograms.Java;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		 Scanner scanner = new Scanner(System.in);
+//	        System.out.print("Enter the number of terms in the Fibonacci series: ");
+//	        int n = scanner.nextInt();
+	        int n = 10;
+	        // Initialize the first two Fibonacci numbers
+	        int first = 0, second = 1;
+	        System.out.println("Fibonacci Series:");
+	        
+	        // Print the first and second Fibonacci numbers
+	        System.out.print(first + " ");
+	        if (n > 1) {
+	            System.out.print(second + " ");
+	        }
+	        
+	        // Compute and print the rest of the Fibonacci numbers
+	        for (int i = 2; i < n; i++) {
+	            int next = first + second;
+	            System.out.print(next + " ");
+	            first = second;
+	            second = next;
+	        }
+	        
+	        //scanner.close();
+
+	}
+
+}
---
+++ src/JavaPackage/ReverseAStringExample.java
@@ -0,0 +1,20 @@
+package JavaPrograms.Java;
+
+public class ReverseAStringExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String inputString = "Hello";
+		String outputString = "";
+
+		// Writing a for loop and looping in the reverse order to get the reverse String
+		// as output.
+
+		for (int i = inputString.length() - 1; i >= 0; i--) {
+			outputString = outputString + inputString.charAt(i);
+		}
+		System.out.println(outputString);
+
+	}
+
+}
